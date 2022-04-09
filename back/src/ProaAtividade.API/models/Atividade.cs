c3b569a5b348fad4445f46b36f9832ddb84194b2
Guilherme <luisguilherme.adm@gmail.com>
2022-04-09 00:09:14 -0300

    ASP.NET 5

---
+++ back/src/ProaAtividade.API/models/Atividade.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProaAtividade.API.models
+{
+    public class Atividade
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Prioridade { get; set; }
+        public Atividade() { }
+        public Atividade(int id)
+        {
+            Id = id;
+        }
+    }
+}
